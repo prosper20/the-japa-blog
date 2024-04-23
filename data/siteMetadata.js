454dc376ffb6d41412e945c38ca7debaf2a93bc3
prosper20 <bobsonconnect@gmail.com>
2024-04-23 18:20:39 +0100

    chore: update preview image

--- data/siteMetadata.js
+++ data/siteMetadata.js
@@ -1,15 +1,15 @@
 /** @type {import("pliny/config").PlinyConfig } */
 const siteMetadata = {
-  title: 'Japa',
+  title: 'Japablog',
   author: 'thejapablog',
   headerTitle: 'thejapablog',
-  description: 'A blog about the japa movement',
+  description: 'A blog dedicated to the japa movement',
   language: 'en-us',
   theme: 'system', // system, dark or light
   siteUrl: 'https://tailwind-nextjs-starter-blog.vercel.app',
   siteRepo: 'https://github.com/timlrx/tailwind-nextjs-starter-blog',
   siteLogo: '/static/images/logo.png',
-  socialBanner: '/static/images/twitter-card.png',
+  socialBanner: '/static/images/japablog-preview.png',
   mastodon: 'https://mastodon.social/@mastodonuser',
   email: 'address@yoursite.com',
   github: 'https://github.com',
